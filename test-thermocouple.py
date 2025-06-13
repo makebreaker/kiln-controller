be9a4113ac788f78af98d3b89f76834f0b3c6c66
makebreaker <66402889+makebreaker@users.noreply.github.com>
2025-06-13 15:47:55 +1000

    ASDF

--- test-thermocouple.py
+++ test-thermocouple.py
@@ -29,32 +29,32 @@ except NotImplementedError:
 # of my thermocouple is .25C.
 ########################################################################
 
-spi = None
-if(hasattr(config,'spi_sclk') and
-   hasattr(config,'spi_mosi') and
-   hasattr(config,'spi_miso')):
-    spi = bitbangio.SPI(config.spi_sclk, config.spi_mosi, config.spi_miso)
-    print("Software SPI selected for reading thermocouple")
-    print("SPI configured as:\n")
-    print("    config.spi_sclk = %s BCM pin" % (config.spi_sclk))
-    print("    config.spi_mosi = %s BCM pin" % (config.spi_mosi))
-    print("    config.spi_miso = %s BCM pin" % (config.spi_miso))
-    print("    config.spi_cs   = %s BCM pin\n" % (config.spi_cs))
-else:
-    spi = board.SPI();
-    print("Hardware SPI selected for reading thermocouple")
+# spi = None
+# if(hasattr(config,'spi_sclk') and
+#    hasattr(config,'spi_mosi') and
+#    hasattr(config,'spi_miso')):
+#     spi = bitbangio.SPI(config.spi_sclk, config.spi_mosi, config.spi_miso)
+#     print("Software SPI selected for reading thermocouple")
+#     print("SPI configured as:\n")
+#     print("    config.spi_sclk = %s BCM pin" % (config.spi_sclk))
+#     print("    config.spi_mosi = %s BCM pin" % (config.spi_mosi))
+#     print("    config.spi_miso = %s BCM pin" % (config.spi_miso))
+#     print("    config.spi_cs   = %s BCM pin\n" % (config.spi_cs))
+# else:
+#     spi = board.SPI();
+#     print("Hardware SPI selected for reading thermocouple")
 
 cs = DigitalInOut(config.spi_cs)
 cs.switch_to_output(value=True)
 sensor = None
 
 print("\nboard: %s" % (board.board_id))
-if(config.max31855):
-    import adafruit_max31855
-    print("thermocouple: adafruit max31855")
-    sensor = adafruit_max31855.MAX31855(spi, cs)
-if(config.max31856):
-    pass
+# if(config.max31855):
+#     import adafruit_max31855
+#     print("thermocouple: adafruit max31855")
+#     sensor = adafruit_max31855.MAX31855(spi, cs)
+# if(config.max31856):
+#     pass
 if(getattr(config, 'mcp9600', 0)):
     import board
     import busio
