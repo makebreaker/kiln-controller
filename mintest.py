3e3b2f5d02d98ea744b4b0f6265a9280317abbb8
makebreaker <66402889+makebreaker@users.noreply.github.com>
2025-06-13 16:06:06 +1000

    asdf

--- mintest.py
+++ mintest.py
@@ -6,7 +6,7 @@ import busio
 import adafruit_mcp9600
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-mcp = adafruit_mcp9600.MCP9600(i2c)
+mcp = adafruit_mcp9600.MCP9600(i2c, address=0x67)
 
 while True:
     print((mcp.ambient_temperature, mcp.temperature, mcp.delta_temperature))
