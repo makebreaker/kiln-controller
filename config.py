b641c7abbdcded03385dbdd11359861ddb4a69d0
makebreaker <66402889+makebreaker@users.noreply.github.com>
2025-06-26 16:56:23 +1000

    asdf

--- config.py
+++ config.py
@@ -126,7 +126,7 @@ seek_start = True
 # Every N seconds a decision is made about switching the relay[s] 
 # on & off and for how long. The thermocouple is read 
 # temperature_average_samples times during and the average value is used.
-sensor_time_wait = 2
+sensor_time_wait = 1
 
 
 ########################################################################
